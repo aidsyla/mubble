51817b61896210e24e9b0b9090c3394d625b1d22
Aid Syla <aid.syla.9@gmail.com>
2025-05-23 03:34:50 +0200

    Yet to implement navigateToOtherProfile, to do later

--- app/src/main/java/com/aidsyla/mubble/feature/profile/ProfileNavigation.kt
+++ app/src/main/java/com/aidsyla/mubble/feature/profile/ProfileNavigation.kt
@@ -4,34 +4,23 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.aidsyla.mubble.common.navigation.OtherProfileRoute
 import com.aidsyla.mubble.common.navigation.ProfileRoute
 
-fun NavController.navigateToOtherProfile(
-    username: String,
-    navOptions: NavOptions? = null,
-) {
-    this.navigate(OtherProfileRoute(username = username), navOptions)
-}
-
 fun NavController.navigateToProfile(
+    userId: String? = null,
     navOptions: NavOptions? = null,
 ) {
-    this.navigate(ProfileRoute, navOptions)
+    this.navigate(ProfileRoute(userId = userId), navOptions)
 }
 
 fun NavGraphBuilder.profileScreen(
     onNavigateToSettings: () -> Unit,
+    onBackClick: () -> Unit,
 ) {
     composable<ProfileRoute> {
-        ProfileScreen(onNavigateToSettings = onNavigateToSettings)
-    }
-}
-
-fun NavGraphBuilder.otherProfileScreen(
-
-) {
-    composable<OtherProfileRoute> {
-        ProfileScreen { }
+        ProfileScreen(
+            onNavigateToSettings = onNavigateToSettings,
+            onBackClick = onBackClick
+        )
     }
 }
