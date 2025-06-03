891cadff8824a438348dac69b887b963a79ee6dc
Aid Syla <aid.syla.9@gmail.com>
2025-06-04 01:51:27 +0200

    Updated all dependencies, removed unused and re organized.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,40 +44,31 @@ android {
 }
 
 dependencies {
-
-    implementation("com.google.dagger:hilt-android:2.56.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.56.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
-
-    implementation(libs.androidx.material.icons.core)
-
-
-    implementation("androidx.navigation:navigation-runtime-ktx:2.8.9")
-
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // For automatic JSON parsing
-    implementation("com.google.code.gson:gson:2.10") // Required for Gson parsing
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation(libs.kotlinx.serialization.json)
-    // Material Components for Android
-    implementation (libs.material)
-    // ViewPager2
-    implementation (libs.androidx.viewpager2)
-    implementation(libs.androidx.foundation)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.foundation)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.ui.text.google.fonts)
-    implementation(libs.androidx.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
