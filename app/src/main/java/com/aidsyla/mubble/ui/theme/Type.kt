131b29464f781cd7586082df14b401e0d3b9d800
Aid Syla <aid.syla.9@gmail.com>
2025-06-12 03:31:26 +0200

    No need to define a default Typography

--- app/src/main/java/com/aidsyla/mubble/ui/theme/Type.kt
+++ app/src/main/java/com/aidsyla/mubble/ui/theme/Type.kt
@@ -1,17 +1,10 @@
 package com.aidsyla.mubble.ui.theme
 
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
-import com.aidsyla.mubble.R
-
-@ExperimentalMaterial3ExpressiveApi
-val baseline = Typography()
 
 // Set of Material typography styles to start with
 val Typography = Typography(
