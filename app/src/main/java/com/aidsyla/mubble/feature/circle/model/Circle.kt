96e1169693d1c22843aa74fa8679ee08d1111542
Aid Syla <aid.syla.9@gmail.com>
2025-06-25 00:39:40 +0200

    Add Circle model

---
+++ app/src/main/java/com/aidsyla/mubble/feature/circle/model/Circle.kt
@@ -0,0 +1,46 @@
+package com.aidsyla.mubble.feature.circle.model
+
+import androidx.annotation.DrawableRes
+import com.aidsyla.mubble.R
+
+data class Circle(
+    val name: String,
+    val memberCount: Int,
+    @DrawableRes
+    val bannerResId: Int,
+)
+
+object CircleRepo {
+    val dummyCircles = listOf(
+        Circle(
+            name = "Nature",
+            memberCount = 1254,
+            bannerResId = R.drawable.circle_1
+        ),
+        Circle(
+            name = "Art",
+            memberCount = 843,
+            bannerResId = R.drawable.circle_2
+        ),
+        Circle(
+            name = "Photography",
+            memberCount = 1920,
+            bannerResId = R.drawable.circle_3
+        ),
+        Circle(
+            name = "Food",
+            memberCount = 1478,
+            bannerResId = R.drawable.circle_4
+        ),
+        Circle(
+            name = "Cars",
+            memberCount = 1083,
+            bannerResId = R.drawable.circle_5
+        ),
+        Circle(
+            name = "Travel",
+            memberCount = 2034,
+            bannerResId = R.drawable.circle_6
+        )
+    )
+}
