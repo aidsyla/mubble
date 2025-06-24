5d98d3f18511e290ad396c7cd95d5e9e8c16ad52
Aid Syla <aid.syla.9@gmail.com>
2025-06-25 00:40:35 +0200

    Update Circle UI design

--- app/src/main/java/com/aidsyla/mubble/common/components/circle/Circle.kt
+++ app/src/main/java/com/aidsyla/mubble/common/components/circle/Circle.kt
@@ -1,5 +1,7 @@
 package com.aidsyla.mubble.common.components.circle
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -10,80 +12,150 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.IconToggleButtonShapes
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconToggleButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.aidsyla.mubble.R
+import com.aidsyla.mubble.feature.circle.model.Circle
+import com.aidsyla.mubble.feature.circle.model.CircleRepo
 import com.aidsyla.mubble.ui.theme.MubbleTheme
+import com.aidsyla.mubble.ui.theme.onSurfaceDark
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AllButton(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier
-            .clip(shape = CircleShape)
-            .size(80.dp)
-            .background(color = MaterialTheme.colorScheme.primary),
-        contentAlignment = Alignment.Center
+    FilledIconButton(
+        onClick = {},
+        modifier =
+            modifier
+                .minimumInteractiveComponentSize()
+                .width(
+                    48.dp
+                ),
+        shapes = IconButtonDefaults.shapes()
     ) {
-        Text(
-            text = "All", style = MaterialTheme.typography.bodyLarge.copy(
-                color = MaterialTheme.colorScheme.onPrimary,
-                fontWeight = FontWeight.SemiBold
-            )
-        )
+        Text("All", style = MaterialTheme.typography.labelLarge)
     }
 }
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun CircleItem(
     modifier: Modifier = Modifier,
-    isFullWidth: Boolean,
-    icon: @Composable () -> Unit = {},
+    circle: Circle,
+    showIcon: Boolean,
 ) {
-    Card(
-        modifier = if (isFullWidth) modifier
-            .fillMaxWidth()
-            .aspectRatio(2f) else modifier.aspectRatio(
-            1f
-        )
+    var checked by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier
+            .clip(MaterialTheme.shapes.medium)
+            .aspectRatio(1.45f)
     ) {
         Image(
-            painter = painterResource(R.drawable.ic_launcher_background),
+            painter = painterResource(circle.bannerResId),
             contentDescription = null,
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
-            contentScale = ContentScale.FillWidth
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(MubbleTheme.Gradients.fadingBlackGradient2)
         )
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 12.dp, vertical = 8.dp)
+            modifier = Modifier
+                .padding(start = 12.dp, end = 4.dp, bottom = 8.dp)
+                .align(Alignment.BottomStart),
         ) {
             Column {
                 Text(
-                    text = "Nature", style = MaterialTheme.typography.titleSmall.copy(
+                    text = circle.name, style = MaterialTheme.typography.titleMedium.copy(
                         fontWeight = FontWeight.Bold
-                    )
+                    ),
+                    color = Color.White
                 )
-                Text(text = "1032 members", style = MaterialTheme.typography.labelSmall)
+                Text(
+                    text = circle.memberCount.toString() + " members",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = onSurfaceDark
+                )
             }
-            Spacer(modifier = Modifier.weight(1f))
-            icon()
+            if (showIcon) {
+                Spacer(modifier = Modifier.weight(1f))
+                OutlinedIconToggleButton(
+                    checked = checked,
+                    onCheckedChange = { checked = it },
+                    modifier =
+                        Modifier
+                            .minimumInteractiveComponentSize()
+                            .size(
+                                IconButtonDefaults.extraSmallContainerSize(
+                                    IconButtonDefaults.IconButtonWidthOption.Wide
+                                )
+                            ),
+                    shapes = IconToggleButtonShapes(
+                        shape = IconButtonDefaults.extraSmallRoundShape,
+                        pressedShape = IconButtonDefaults.extraSmallPressedShape,
+                        checkedShape = IconButtonDefaults.extraSmallSquareShape
+                    ),
+                    colors = IconButtonDefaults.outlinedIconToggleButtonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.White,
+                        checkedContainerColor = Color.Transparent,
+                        checkedContentColor = Color.White
+                    ),
+                    border = BorderStroke(1.dp, Color.White)
+                ) {
+                    AnimatedContent(
+                        targetState = checked
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(
+                                IconButtonDefaults.extraSmallIconSize
+                            ),
+                            imageVector = if (it) Icons.Default.Check else Icons.Default.Add,
+                            contentDescription = null,
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
         }
+    }
+}
+
+val circle = CircleRepo.dummyCircles.component2()
+
+@Preview
+@Composable
+private fun AllButtonPreview() {
+    MubbleTheme {
+        AllButton()
     }
 }
 
@@ -91,11 +163,7 @@ fun CircleItem(
 @Composable
 private fun WidePreview() {
     MubbleTheme {
-        CircleItem(isFullWidth = true) {
-            FilledIconButton(onClick = {}) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = null)
-            }
-        }
+        CircleItem(circle = circle, showIcon = true)
     }
 }
 
@@ -103,6 +171,6 @@ private fun WidePreview() {
 @Composable
 private fun SmallPreview() {
     MubbleTheme {
-        CircleItem(isFullWidth = false)
+        CircleItem(circle = circle, showIcon = false)
     }
 }
