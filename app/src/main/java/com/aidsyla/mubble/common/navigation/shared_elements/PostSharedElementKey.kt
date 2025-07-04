af03cc49446d13ea22223faf3605e0ab3e818e2c
Aid Syla <aid.syla.9@gmail.com>
2025-07-04 18:55:34 +0200

    Create PostSharedElementKey for shared element transitions

---
+++ app/src/main/java/com/aidsyla/mubble/common/navigation/shared_elements/PostSharedElementKey.kt
@@ -0,0 +1,26 @@
+package com.aidsyla.mubble.common.navigation.shared_elements
+
+data class PostSharedElementKey(
+    val postId: String,
+    val origin: PostOrigin,
+    val type: PostSharedElementType,
+)
+
+enum class PostOrigin {
+    None,
+    HomeFollowing,
+    HomeMyCircles,
+    ExploreMedia,
+    ExploreBubbles,
+    ProfileMedia,
+    ProfileBubbles
+}
+
+enum class PostSharedElementType {
+    ProfileAvatar,
+    Bounds,
+    DisplayName,
+    Image,
+    Video,
+    Bubble
+}
