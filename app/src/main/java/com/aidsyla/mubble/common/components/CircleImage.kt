ca1ab5b2923b0b8b2f88de9c8447921f700b9dc9
Aid Syla <aid.syla.9@gmail.com>
2025-07-14 03:09:39 +0200

    Add borderColor parameter

--- app/src/main/java/com/aidsyla/mubble/common/components/CircleImage.kt
+++ app/src/main/java/com/aidsyla/mubble/common/components/CircleImage.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
@@ -19,6 +20,7 @@ fun CircleImage(
     painter: Painter,
     size: Dp = 48.dp,
     borderWidth: Dp = 1.dp,
+    borderColor: Color = MaterialTheme.colorScheme.outlineVariant,
     contentDescription: String? = null,
 ) {
     val imageModifier = modifier
@@ -26,7 +28,7 @@ fun CircleImage(
         .clip(CircleShape)
         .then(
             if (borderWidth > 0.dp) {
-                Modifier.border(borderWidth, MaterialTheme.colorScheme.outlineVariant, CircleShape)
+                Modifier.border(borderWidth, borderColor, CircleShape)
             } else {
                 Modifier
             }
