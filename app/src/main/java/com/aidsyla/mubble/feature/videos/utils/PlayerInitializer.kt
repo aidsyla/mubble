5e3d6f7edb06ebf5b8aab5d8f52f126ac80baf9a
Aid Syla <aid.syla.9@gmail.com>
2025-07-14 03:29:50 +0200

    Add logic to initialize player with custom load control

---
+++ app/src/main/java/com/aidsyla/mubble/feature/videos/utils/PlayerInitializer.kt
@@ -0,0 +1,36 @@
+package com.aidsyla.mubble.feature.videos.utils
+
+import android.content.Context
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.upstream.DefaultAllocator
+
+private const val MIN_BUFFER_MS = 15_000
+private const val MAX_BUFFER_MS = 15_000
+private const val BUFFER_FOR_PLAYBACK_MS = 1_000
+private const val BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS = 2_000
+
+@androidx.annotation.OptIn(UnstableApi::class)
+internal fun initializePlayerForVideo(context: Context, videoUrl: String): Player {
+    val loadControl = DefaultLoadControl.Builder()
+        .setAllocator(DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE))
+        .setBufferDurationsMs(
+            MIN_BUFFER_MS,
+            MAX_BUFFER_MS,
+            BUFFER_FOR_PLAYBACK_MS,
+            BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
+        )
+        .setTargetBufferBytes(C.LENGTH_UNSET)
+        .setPrioritizeTimeOverSizeThresholds(true)
+        .build()
+    return ExoPlayer.Builder(context)
+        .setLoadControl(loadControl)
+        .build().apply {
+            setMediaItem(MediaItem.fromUri(videoUrl))
+            prepare()
+        }
+}
