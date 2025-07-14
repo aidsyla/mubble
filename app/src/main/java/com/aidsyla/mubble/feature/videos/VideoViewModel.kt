216e08f23fcb9e0eaef4c555df0f0405ddd09fbf
Aid Syla <aid.syla.9@gmail.com>
2025-07-14 03:30:59 +0200

    Add VideoViewModel to hold ui state

---
+++ app/src/main/java/com/aidsyla/mubble/feature/videos/VideoViewModel.kt
@@ -0,0 +1,60 @@
+package com.aidsyla.mubble.feature.videos
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoViewModel @Inject constructor(
+) : ViewModel() {
+
+    private val videoListOne =
+        listOf(
+            "https://html5demos.com/assets/dizzy.mp4",
+            "https://storage.googleapis.com/exoplayer-test-media-0/shortform_2.mp4",
+            "https://storage.googleapis.com/exoplayer-test-media-1/gen-3/screens/dash-vod-single-segment/video-vp9-360.webm",
+            "https://storage.googleapis.com/exoplayer-test-media-0/shortform_3.mp4",
+        )
+
+    private val videoListTwo =
+        listOf(
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
+        )
+
+    private val _uiState = MutableStateFlow<VideoScreenUiState>(VideoScreenUiState.Loading)
+    val uiState: StateFlow<VideoScreenUiState> = _uiState.asStateFlow()
+
+    init {
+        loadVideos()
+    }
+
+    private fun loadVideos() {
+        viewModelScope.launch {
+            _uiState.value = VideoScreenUiState.Loading
+            delay(500)
+            _uiState.value = VideoScreenUiState.Success(videoListOne)
+        }
+    }
+}
+
+sealed interface VideoScreenUiState {
+    data object Loading : VideoScreenUiState
+    data class Success(val videoUrls: List<String>) : VideoScreenUiState
+}
