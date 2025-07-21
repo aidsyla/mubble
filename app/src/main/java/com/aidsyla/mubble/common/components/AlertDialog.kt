9e8afec0075767aaf094bb71ea91a02e0ac2d9c0
Aid Syla <aid.syla.9@gmail.com>
2025-07-22 01:19:50 +0200

    Add AlertDialog composable

---
+++ app/src/main/java/com/aidsyla/mubble/common/components/AlertDialog.kt
@@ -0,0 +1,47 @@
+package com.aidsyla.mubble.common.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
+
+@Composable
+fun AlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: AnnotatedString,
+    confirmButtonText: String,
+) {
+    AlertDialog(
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            FilledTonalButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text(text = confirmButtonText)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text(text = "Cancel")
+            }
+        }
+    )
+}
