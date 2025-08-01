61f2b7f3d3fa3419d8e38601d3b06ae76f20d264
Aid Syla <aid.syla.9@gmail.com>
2025-08-01 03:31:09 +0200

    Create SubtleHorizontalDivider

---
+++ app/src/main/java/com/aidsyla/mubble/common/components/SubtleHorizontalDivider.kt
@@ -0,0 +1,22 @@
+package com.aidsyla.mubble.common.components
+
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SubtleHorizontalDivider(
+    modifier: Modifier = Modifier,
+    thickness: Dp = 0.5.dp,
+    color: Color = DividerDefaults.color.copy(alpha = 0.5f),
+) {
+    HorizontalDivider(
+        modifier = modifier,
+        thickness = thickness,
+        color = color
+    )
+}
