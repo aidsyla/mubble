7441db57ccb9ef3ecae5dcb12ab6d31652f07379
Aid Syla <aid.syla.9@gmail.com>
2025-08-01 03:45:42 +0200

    Update ChatScreen Bottom Bar and UI

--- app/src/main/java/com/aidsyla/mubble/feature/chats/ChatScreen.kt
+++ app/src/main/java/com/aidsyla/mubble/feature/chats/ChatScreen.kt
@@ -3,31 +3,29 @@ package com.aidsyla.mubble.feature.chats
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.MailOutline
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -40,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -47,6 +46,8 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.aidsyla.mubble.R
+import com.aidsyla.mubble.common.components.CircleImage
+import com.aidsyla.mubble.ui.theme.MubbleTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,69 +66,47 @@ fun ChatScreen(
     when (val state = uiState) {
         ChatScreenUiState.Loading -> {}
         is ChatScreenUiState.Success -> {
-            Scaffold(topBar = {
-                TopAppBar(
-                    title = {
-                        Row(
-                            modifier = Modifier.clickable {
-                                onProfileClick(state.otherUser.id)
-                            },
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            Image(
-                                painter = painterResource(
-                                    id = state.otherUser.profilePictureResId
-                                ),
-                                contentDescription = "${state.otherUser.displayName} profile picture",
-                                modifier = Modifier
-                                    .size(40.dp)
-                                    .clip(CircleShape),
-                                contentScale = ContentScale.Crop,
-                            )
-                            Text(
-                                text = state.otherUser.displayName,
-                                style = MaterialTheme.typography.titleMedium
-                            )
-                        }
-                    }, navigationIcon = {
-                        IconButton(onClick = onBackClick) {
-                            Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
-                        }
-                    }, actions = {
-                        IconButton(onClick = { onMoreClick(state.otherUser.id) }) {
-                            Icon(Icons.Filled.MoreVert, "More options")
-                        }
-                    }, scrollBehavior = scrollBehavior
-                )
-            }, bottomBar = {
-                BottomAppBar(
-                    modifier = Modifier.clip(
-                        shape = RoundedCornerShape(
-                            topStart = 12.dp, topEnd = 12.dp
-                        )
-                    ),
-                    contentPadding = PaddingValues(12.dp),
-                ) {
-                    Box(
-                        contentAlignment = Alignment.CenterStart,
-                        modifier = Modifier
-                            .weight(1f)
-                            .fillMaxHeight()
-                            .clip(shape = RoundedCornerShape(50.dp))
-                            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
-                            .padding(horizontal = 16.dp, vertical = 8.dp)
-                    ) {
-                        Text(
-                            "Type your message...", style = MaterialTheme.typography.bodyMedium
-                        )
-                    }
-                    IconButton(
-                        onClick = {}) {
-                        Icon(imageVector = Icons.Default.MailOutline, contentDescription = null)
-                    }
-                }
-            }) {
+            Scaffold(
+                modifier = modifier,
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Row(
+                                modifier = Modifier.clickable {
+                                    onProfileClick(state.otherUser.id)
+                                },
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                CircleImage(
+                                    painter = painterResource(
+                                        id = state.otherUser.profilePictureResId
+                                    ),
+                                    contentDescription = "${state.otherUser.displayName} profile picture",
+                                    size = 40.dp,
+                                    borderWidth = 0.5.dp
+                                )
+                                Text(
+                                    text = state.otherUser.displayName,
+                                    style = MaterialTheme.typography.titleMedium
+                                )
+                            }
+                        }, navigationIcon = {
+                            IconButton(onClick = onBackClick) {
+                                Icon(painter = MubbleTheme.Icons.ArrowBack, "Back")
+                            }
+                        }, actions = {
+                            IconButton(onClick = { onMoreClick(state.otherUser.id) }) {
+                                Icon(MubbleTheme.Icons.MoreHorizontal, "More options")
+                            }
+                        }, scrollBehavior = scrollBehavior
+                    )
+                }, bottomBar = {
+                    MessageBottomBar()
+                },
+                containerColor = Color.Unspecified
+            )
+            {
                 LazyColumn(
                     state = listState,
                     reverseLayout = true,
@@ -135,6 +114,7 @@ fun ChatScreen(
                         .fillMaxSize()
                         .padding(it)
                         .nestedScroll(scrollBehavior.nestedScrollConnection),
+//                        .background(brush = MubbleTheme.Gradients.appGradient),
                     contentPadding = PaddingValues(8.dp),
                     verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.Bottom)
                 ) {
@@ -149,6 +129,78 @@ fun ChatScreen(
                     listState.scrollToItem(index = 0)
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun MessageBottomBar(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.surfaceContainerLow,
+) {
+    Surface(
+        modifier = modifier,
+        color = color,
+    ) {
+        Row(
+            modifier = Modifier
+                .height(80.dp)
+                .padding(start = 8.dp, end = 4.dp)
+                .padding(vertical = 4.dp)
+                .navigationBarsPadding(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(40.dp)
+                    .clip(shape = RoundedCornerShape(100))
+                    .border(
+                        width = 0.5.dp,
+                        color = MaterialTheme.colorScheme.outlineVariant,
+                        shape = RoundedCornerShape(100)
+                    )
+                    .background(color = MaterialTheme.colorScheme.surfaceContainerHighest),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Row(
+                    modifier = Modifier.padding(start = 4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    FilledIconButton(
+                        onClick = {},
+                        modifier = Modifier.size(32.dp)
+                    ) {
+                        Icon(
+                            painter = MubbleTheme.Icons.Camera,
+                            contentDescription = null
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.padding(start = 8.dp, end = 4.dp),
+                        text = "Message...",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+            }
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    painter = MubbleTheme.Icons.Gallery,
+                    contentDescription = null
+                )
+            }
+//            IconButton(
+//                onClick = {}
+//            ) {
+//                Icon(
+//                    painter = MubbleTheme.Icons.Add,
+//                    contentDescription = null
+//                )
+//            }
         }
     }
 }
@@ -204,7 +256,7 @@ fun ChatImage(modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(R.drawable.ic_launcher_background),
         contentDescription = null,
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth(0.5f)
             .clip(
                 shape = RoundedCornerShape(20.dp)
